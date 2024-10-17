4b25fda2a60efcb4d740a6d80e5f1682ce34682b
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-10-17 13:40:06 -0500

    Creacion de metodos Crud :construction:

--- ListaTareas/ListadeTarea.java
+++ ListaTareas/ListadeTarea.java
@@ -1,5 +1,23 @@
 package ListaTareas;
 
+import java.util.ArrayList;
+
 public class ListadeTarea {
+    ArrayList<Tarea> listaTareas = new ArrayList<>();
+
+    public void AgregarTarea(Tarea tarea){
+        listaTareas.add(tarea);
+    }
     
+    public void TraerTarea(){
+
+    }
+
+    public void Actualizar(){
+
+    }
+
+    public void Eliminar(){
+        
+    }
 }
