efb42f0789c01e2ae904c2e0b3fcc203c3409c4c
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-11-07 17:09:52 -0500

    Prueba #1

--- Proyectos Backend/Biblioteca/Main.java
+++ Proyectos Backend/Biblioteca/Main.java
@@ -1,8 +1,43 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    ArrayList<String> prubas  = new ArrayList<String>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        while (opcion != 8) {
+            System.out.println("¿Qué desea hacer?");
+            System.out.println("1. Registrar socio\n2. Registrar libro\n3. Registrar prestamo\n4. Ver socios\n5. Ver libros\n6. Ver prestamos\n7. Ver socios no fiables\n8. Salir\nElige:");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    Socios.CrearSocio();
+                    break;
+                case 2:
+                    Libros.CrearLibro();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    Socios.ImprimirSocios();
+                    break;
+                case 5:
+                    Libros.ImprimirLibro();
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    System.out.println("Muchas Gracias Bye.");
+                    break;
+                default:
+                    System.out.println("Aprenda a Leer el Menu.");
+                    break;
+            }
+        }
+        sc.close();
+    }
     
     
     
