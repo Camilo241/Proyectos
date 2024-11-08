e600e5cb2c066c62e7aa578598ea6ed711cfcbff
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-11-07 19:31:21 -0500

    Actulizando Menu y realizando cambios para la Prueba 2 del Crud en libros y socios :rocket:

--- Proyectos Backend/Biblioteca/Main.java
+++ Proyectos Backend/Biblioteca/Main.java
@@ -7,7 +7,8 @@ public class Main {
         int opcion = 0;
         while (opcion != 8) {
             System.out.println("¿Qué desea hacer?");
-            System.out.println("1. Registrar socio\n2. Registrar libro\n3. Registrar prestamo\n4. Ver socios\n5. Ver libros\n6. Ver prestamos\n7. Ver socios no fiables\n8. Salir\nElige:");
+            System.out.println(
+                    "1. Registrar socio\n2. Registrar libro\n3. Registrar prestamo\n4. Ver socios\n5. Ver libros\n6. Ver prestamos\n7. Ver socios no fiables\n8. Actualizar Socio\n9. Actualizar Libro\n10. Actualizar Prestamo\n11. Eliminar Socio\n12. Eliminar Libro\n13. Eliminar Prestamo\n14. Salir \nElige:");
             opcion = sc.nextInt();
             sc.nextLine();
             switch (opcion) {
@@ -30,6 +31,23 @@ public class Main {
                 case 7:
                     break;
                 case 8:
+                    Socios.ActualizarSocio(sc);
+                    break;
+                case 9:
+                    Libros.ActualizarLibro(sc);
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    Socios.EliminarSocio(sc);
+                    break;
+                case 12:
+                    Libros.EliminarLibro(sc);
+                    break;
+                case 13:
+                    Socios.ActualizarSocio(sc);
+                    break;
+                case 14:
                     System.out.println("Muchas Gracias Bye.");
                     break;
                 default:
@@ -39,7 +57,5 @@ public class Main {
         }
         sc.close();
     }
-    
-    
-    
+
 }
