b45fbc89c5612fd373cb1c3475133245e75504aa
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-11-11 14:21:46 -0500

    Sigo con algunas Dudas :construction:

---
+++ Proyectos Backend/Metodos de Busqueda/Burbuja.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Burbuja {
+    public static void Ordenamiento(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] > array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    
+}
