48735dc52da63e392c11582cec8ce7521f64e1d7
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-11-13 13:28:21 -0500

    siempre los ordeno al reves jaja :rocket:

--- Proyectos Backend/Metodos de Busqueda/Seleccion.java
+++ Proyectos Backend/Metodos de Busqueda/Seleccion.java
@@ -1,3 +1,19 @@
+import java.util.Arrays;
+
 public class Seleccion {
-    
+    // 4,9,8,3,7,6,15,2
+    public static void Ordenamiento(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int menor = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[menor]) {
+                    menor = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[menor];
+            array[menor] = temp;
+            System.out.println(Arrays.toString(array));
+        }
+    }
 }
