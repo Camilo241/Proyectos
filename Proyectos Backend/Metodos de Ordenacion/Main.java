52569fb9faf868a69a29b91db32022a8190ab423
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-11-15 14:34:31 -0500

    Cambio de Nombre de Carpeta  :bug:

--- Proyectos Backend/Metodos de Ordenacion/Main.java
+++ Proyectos Backend/Metodos de Ordenacion/Main.java
@@ -3,8 +3,8 @@
 public class Main {
     public static void main(String[] args){
         int[] array = {4,9,8,3,7,6,15,2};
-        // Burbuja.Ordenamiento(array);
-        // Seleccion.Ordenamiento(array);
+        Burbuja.Ordenamiento(array);
+        Seleccion.Ordenamiento(array);
         OrdenacionRapida.Ordenamiento(array);
     }
 }
