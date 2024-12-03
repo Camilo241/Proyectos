a2678d8c991b3687944e9210b1622896364f4f95
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-12-03 14:11:07 -0500

    :white_check_mark:

--- Proyectos Backend/Hoteleria/Main.java
+++ Proyectos Backend/Hoteleria/Main.java
@@ -1,5 +1,6 @@
 package Hoteleria;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,11 +8,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Habitaciones> listaHabitaciones = new ArrayList<>();
+        ArrayList<Reservas> listaReservasiones = new ArrayList<>();
+
         int idCounter = 1;
         int opcion = 0;
         do {
             System.out.println(
-                    "Seleccione una opción: \n 1. Crear Habitación\n 2. Actualizar Habitación\n 3. Eliminar Habitación\n 4. Ver Habitaciones\n 5. Salir");
+                    "Seleccione una opción: \n 1. Crear Habitación\n 2. Actualizar Habitación\n 3. Eliminar Habitación\n 4. Ver Habitaciones\n 5. Crear Reserva \n 6. Actualizar Reserva\n 7. Eliminar Reserva \n 8. Ver Reservas \n 9.Salir");
             System.out.print("Ingrese su opción: ");
             opcion = sc.nextInt();
             sc.nextLine();
@@ -33,7 +36,7 @@ public class Main {
                 case 2:
                     System.out.println("Ingrese el ID de la habitación a actualizar:");
                     int idActualizar = sc.nextInt();
-                    sc.nextLine(); // Consumir salto de línea
+                    sc.nextLine();
                     Habitaciones habitacion = Habitaciones.buscarHabitacionPorId(idActualizar, listaHabitaciones);
                     if (habitacion != null) {
                         System.out.println("Ingrese el nuevo nombre (dejar vacío para no cambiar):");
@@ -60,13 +63,73 @@ public class Main {
                     System.out.println("Eliminar Habitación seleccionada.");
                     Habitaciones.EliminarHabitaciones(idEliminar, listaHabitaciones);
                     break;
-
                 case 4:
                     System.out.println("Ver todas las Habitaciones.");
                     Habitaciones.VerHabitraciones(listaHabitaciones);
                     break;
-
                 case 5:
+                    System.out.println("Ingrese el ID de la habitación que desea reservar:");
+                    int idHabitacion = sc.nextInt();
+                    Habitaciones habitacionSeleccionada = null;
+
+                    // Buscar habitación por ID
+                    for (Habitaciones h : listaHabitaciones) {
+                        if (h.getId() == idHabitacion) {
+                            habitacionSeleccionada = h;
+                            break;
+                        }
+                    }
+                    if (habitacionSeleccionada != null) {
+                        System.out.println("Ingrese su Nombre:");
+                        String nombreU = sc.next();
+                        System.out.println("Ingrese la Fecha de entrada (AAAA-MM-DD):");
+                        String fechaEntrada = sc.next();
+                        LocalDate fechaReservaE = LocalDate.parse(fechaEntrada);
+                        System.out.println("Ingrese la Fecha de salida (AAAA-MM-DD)");
+                        String fechaSalida = sc.next();
+                        LocalDate fechaReservaS = LocalDate.parse(fechaSalida);
+                        sc.nextLine();
+
+                        Reservas nuevaReserva = new Reservas(idHabitacion, nombreU, habitacionSeleccionada, fechaReservaE,fechaReservaS);
+                        listaReservasiones.add(nuevaReserva);
+                    }else{
+                        System.out.println("Id no es Valido.");
+                    }
+                    break;
+                case 6:
+                    // System.out.println("Ingrese el ID de la habitación a actualizar:");
+                    // int idActualizar = sc.nextInt();
+                    // sc.nextLine(); // Consumir salto de línea
+                    // Habitaciones habitacion = Habitaciones.buscarHabitacionPorId(idActualizar, listaHabitaciones);
+                    // if (habitacion != null) {
+                    //     System.out.println("Ingrese el nuevo nombre (dejar vacío para no cambiar):");
+                    //     String nuevoNombre = sc.nextLine();
+                    //     if (!nuevoNombre.isEmpty()) {
+                    //         habitacion.setNombre(nuevoNombre);
+                    //     }
+
+                    //     System.out.println("Ingrese el nuevo precio (0 para no cambiar):");
+                    //     int nuevoPrecio = sc.nextInt();
+                    //     if (nuevoPrecio > 0) {
+                    //         habitacion.setPrecio(nuevoPrecio);
+                    //     }
+                    //     System.out.println("Habitación actualizada.");
+                    // } else {
+                    //     System.out.println("Habitación no encontrada.");
+                    // }
+                    // break;
+                case 7:
+                    System.out.println("Ingrese el ID de la habitación a Eliminar:");
+                    int idEliminarR = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Eliminar Habitación seleccionada.");
+                    Habitaciones.EliminarHabitaciones(idEliminarR, listaHabitaciones);
+                    break;
+                case 8:
+                    System.out.println("Ver todas las Reservaciones.");
+                    Reservas.VerReservaHabitacion(listaReservasiones);
+                    break;
+                case 9:
                     System.out.println("Saliendo del programa...");
                     break;
 
@@ -78,7 +141,7 @@ public class Main {
                 System.out.println("\nPresione Enter para continuar...");
                 sc.nextLine();
             }
-        } while (opcion != 5);
+        } while (opcion != 9);
 
         sc.close();
     }
--- Proyectos Backend/Hoteleria/Reservas.java
+++ Proyectos Backend/Hoteleria/Reservas.java
@@ -1,13 +1,59 @@
 package Hoteleria;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Reservas {
-    ate FechaInicio,FechaFinal;
-    static void CrearRerservarHabitacion(){}
-    static void ActualizarReservaHabitacio(){}
-    static void EliminarReservaHabitacio(){}
-    static void VerReservaHabitacio(){}
+    int ID;
+    String Nombre;
+    Habitaciones Habitaciones;
+    LocalDate FechaInicio,FechaFinal;
+
+    static void EliminarReservaHabitacion(int ID,ArrayList <Reservas> listaReservasiones){
+        listaReservasiones.remove(ID);
+    }
+    static void VerReservaHabitacion(ArrayList <Reservas> listaReservasiones){
+        for (Reservas reservas : listaReservasiones) {
+            System.out.println("ID: " + reservas.getID() + "\nNombre de la persona que reservo: " + reservas.getNombre() + "\nFecha de Entrada: " + reservas.getFechaInicio() + "\nFecha de Salida: " + reservas.getFechaFinal());
+        }
+    }
+
+
+    public Reservas(int iD, String nombre,Habitaciones habitaciones, LocalDate fechaInicio, LocalDate fechaFinal) {
+        ID = iD;
+        Nombre = nombre;
+        Habitaciones = habitaciones;
+        FechaInicio = fechaInicio;
+        FechaFinal = fechaFinal;
+    }
+    public int getID() {
+        return ID;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public Habitaciones getHabitaciones() {
+        return Habitaciones;
+    }
+    public LocalDate getFechaInicio() {
+        return FechaInicio;
+    }
+    public void setFechaInicio(LocalDate fechaInicio) {
+        FechaInicio = fechaInicio;
+    }
+    public LocalDate getFechaFinal() {
+        return FechaFinal;
+    }
+    public void setFechaFinal(LocalDate fechaFinal) {
+        FechaFinal = fechaFinal;
+    }
+    
     
 
 }
