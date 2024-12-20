642c48fdfa41084ea9e56e3559dfd582a551b6bc
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-12-19 21:59:35 -0500

    Creacion de Categoria :construction:

--- Proyectos Backend/ListaTareas/Categoria.java
+++ Proyectos Backend/ListaTareas/Categoria.java
@@ -1,51 +1,19 @@
-import java.util.ArrayList;
+
 
 public class Categoria {
-    private String nombre;
-    ArrayList<String> Categorias = new ArrayList<>();
+    private int Id; 
+    private String Nombre;
+    
     public Categoria(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void CrearCategorias(String nombre){
-        Categorias.add(nombre);
-    }
-
-    public void LeerCategorias(){
-        for (String c : Categorias) {
-            System.out.println(c);
-        }
-    }
-
-    public void ActualizarCategoria(int indice){
-        for (int i = 0; i < Categorias.size(); i++) {
-            if(indice == i){
-                setNombre(nombre);
-            }else{
-                System.out.println("El indice no fue encontrado");
-            }
-        }
-    }
-
-    public void EliminarCategria(int indice){
-        for (int j = 0; j < Categorias.size(); j++) {
-            if (indice == j) {
-                Categorias.remove(indice);
-            }else{
-                System.out.println("El indice no fue encontrado");
-            }
-            
-        }
-        
-
+        Nombre = nombre;
     }
 
     public String getNombre() {
-        return nombre;
+        return Nombre;
     }
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        Nombre = nombre;
     }
     
 }
