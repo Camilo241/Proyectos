b670cfafe07a88d3bdface7b93161253122c2426
Kmilo160702 <camilo160702@hotmail.com>
2025-02-22 13:27:22 -0500

    :art: LDT-10: Formato y correccion de los static

--- Proyectos Backend/ListaTareas/Controladores/ListadeTarea.java
+++ Proyectos Backend/ListaTareas/Controladores/ListadeTarea.java
@@ -1,6 +1,5 @@
 package Controladores;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,21 +9,21 @@ import java.util.List;
 
 import database.ConexionSQL;
 
-
-
 public class ListadeTarea {
-    public static void insertarTarea(String nombre, String descripcion, int categoriaId, String estado) {
+    public static void insertarTarea(String nombre, String descripcion, int categoriaId, String estado) { // Porque
+                                                                                                          // metodos
+                                                                                                          // STATIC?
         String sql = "INSERT INTO tareas (nombre, descripcion, id_categoria, estado) VALUES (?, ?, ?, ?)";
 
         try (Connection conn = ConexionSQL.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setString(1, nombre);
             stmt.setString(2, descripcion);
             stmt.setInt(3, categoriaId);
             stmt.setString(4, estado);
             stmt.executeUpdate();
-            
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -35,9 +34,9 @@ public class ListadeTarea {
         String sql = "SELECT nombre FROM tareas";
 
         try (Connection conn = ConexionSQL.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql);
-             ResultSet rs = stmt.executeQuery()) {
-            
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+
             while (rs.next()) {
                 tareas.add(rs.getString("nombre"));
             }
@@ -47,9 +46,5 @@ public class ListadeTarea {
 
         return tareas;
     }
-    
 
-    
-
-    
 }
