8c53c3774b5861ffa0bc375ae333ab2400ce7f14
Kmilo160702 <camilo160702@hotmail.com>
2025-02-22 13:27:33 -0500

    :art: LDT-10: Formato

--- Proyectos Backend/ListaTareas/Main/MainListaTareas.java
+++ Proyectos Backend/ListaTareas/Main/MainListaTareas.java
@@ -7,57 +7,52 @@ import Controladores.ListadeCategorias;
 import Controladores.ListadeTarea;
 import database.ConexionSQL;
 
-
-
 public class MainListaTareas {
     public static void main(String[] args) {
         Connection conn = ConexionSQL.conectar();
         if (conn != null) {
             System.out.println("✅ Conexión exitosa a la base de datos.");
             try {
-                conn.close(); 
+                conn.close();
             } catch (SQLException e) {
                 System.out.println("Error al cerrar la conexión: " + e.getMessage());
             }
         } else {
             System.out.println("❌ Error en la conexión.");
-        }// validamos la coneccion y mandamos el mensaje
+        } // validamos la coneccion y mandamos el mensaje
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa el nombre de la categoria.");
         String nombreCategoria = sc.next();
         System.out.println("Ingresa el nombre de la tarea.");
         String nombre = sc.nextLine();
-        
+
         System.out.println("Ingresa la descripcion de la tarea.");
         String descripcion = sc.nextLine();
         System.out.println("Ingresa el id de la categoria.");
         int categoria = sc.nextInt();
-        
+
         // System.out.println("Ingresa el estado de la Tarea.");
         // String estado = sc.nextLine();
         sc.next();
         ListadeCategorias listacategorias = new ListadeCategorias();
         ListadeTarea listatarea = new ListadeTarea();
 
-                        //Categorias
-        //Create
+        // Categorias
+        // Create
         ListadeCategorias.insertarCategoria(nombreCategoria);
-        //Read
+        // Read
         System.out.println("Categorías: " + ListadeCategorias.obtenerCategorias());
-        //Update
-        //Delete
+        // Update
+        // Delete
 
-                        //Tareas
-        //Create
-        ListadeTarea.insertarTarea(nombre,descripcion,categoria,null);
-        //Read
+        // Tareas
+        // Create
+        ListadeTarea.insertarTarea(nombre, descripcion, categoria, null);
+        // Read
         System.out.println("Tareas: " + ListadeTarea.obtenerTareas());
-        //Update
-        //Delete
+        // Update
+        // Delete
 
-        
-
-    
     }
 }
